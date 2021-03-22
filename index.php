406c855014f71f96e50352368ea644283bf2bd81
DeDomin98 <75976221+DeDomin98@users.noreply.github.com>
2021-03-22 11:01:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php 
 include_once ("Index.html");
-include_once ("cv.html");
-include_once ("kimjestem.html");
-include_once ("kontakt.html");
-include_once ("oferta.html");
-include_once ("demo.html");
+
 
 ?>
